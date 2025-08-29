facc4427982290f6d981b5e4f31ef3bbdc4fa6dd
Kris Rowe <kris.rowe@rowelink.com>
2025-08-28 23:38:31 -0500

    Add missing __init__.py for run package
    - Fixes ModuleNotFoundError when importing from multi_eden.run
    - Makes run directory a proper Python package

---
+++ src/multi_eden/run/__init__.py
@@ -0,0 +1,6 @@
+"""
+Multi-Eden Runtime Package
+
+This package contains runtime components for Multi-Eden applications,
+including configuration, API base classes, and other runtime utilities.
+"""
